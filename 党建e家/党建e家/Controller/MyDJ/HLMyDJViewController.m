b3e45393e26f1e6e7335ac8547285eef9d943394
何龙 <hlSkate@sina.cn>
2018-10-25 12:04:18 +0800

    这是爸爸搭的框架

---
+++ 党建e家/党建e家/Controller/MyDJ/HLMyDJViewController.m
@@ -0,0 +1,32 @@
+//
+//  HLMyDJViewController.m
+//  党建e家
+//
+//  Created by 何龙 on 2018/10/25.
+//  Copyright © 2018 何龙. All rights reserved.
+//
+
+#import "HLMyDJViewController.h"
+
+@interface HLMyDJViewController ()
+
+@end
+
+@implementation HLMyDJViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
