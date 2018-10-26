891918003703819bf69dc11315f8becd89857ae3
黄钦 <1823872927@qq.com>
2018-10-26 10:11:47 +0800

    no message

--- 党建e家/党建e家/Controller/MyDJ/View/View.m
+++ 党建e家/党建e家/Controller/MyDJ/View/View.m
@@ -15,42 +15,29 @@
     if (self = [super initWithFrame:frame])
     {
         self.backgroundColor = Color(182, 0, 10);
-        _imageview = [[UIImageView alloc]init];
-        _imageview.image = [UIImage imageNamed:@"logo"];
-        [self addSubview:_imageview];
+        _imageview = [UIImageView imageWithImage:@"logo" subView:self];
         [_imageview mas_makeConstraints:^(MASConstraintMaker *make) {
             make.top.equalTo(0);
             make.centerX.equalTo(self.centerX);
             make.width.equalTo(180);
             make.height.equalTo(60);
         }];
-        _nameimageview = [[UIImageView alloc]init];
-        _nameimageview.image = [UIImage imageNamed:@"登录框"];
-        [self addSubview:_nameimageview];
+        
+        _nameimageview = [UIImageView imageWithImage:@"登录框" subView:self];
         [_nameimageview mas_makeConstraints:^(MASConstraintMaker *make) {
             make.top.equalTo(self.imageview.bottom).offset(33);
             make.left.equalTo(30);
             make.right.equalTo(-30);
             make.height.equalTo(39);
         }];
-        _passimageview = [[UIImageView alloc]init];
-        _passimageview.image = [UIImage imageNamed:@"登录框"];
-        [self addSubview:_passimageview];
+        _passimageview = [UIImageView imageWithImage:@"登录框" subView:self];
         [_passimageview mas_makeConstraints:^(MASConstraintMaker *make) {
             make.top.equalTo(self.nameimageview.bottom).offset(8);
             make.left.equalTo(30);
             make.right.equalTo(-30);
             make.height.equalTo(39);
         }];
-        _loginbnt = [UIButton buttonWithType:UIButtonTypeCustom];
-//        [_loginbnt setImage:[UIImage imageNamed:@"登录btn"] forState:UIControlStateNormal];
-        _loginbnt.backgroundColor = [UIColor lightGrayColor];
-        [_loginbnt setTitle:@"登录" forState:UIControlStateNormal];
-        _loginbnt.titleLabel.font = [UIFont systemFontOfSize:13];
-        _loginbnt.layer.cornerRadius = 5;
-        _loginbnt.layer.masksToBounds = YES;
-        [_loginbnt setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        [self addSubview:_loginbnt];
+        _loginbnt = [UIButton_UIButton_HuangQinCategory bntWithBGcorol:Color(225, 88, 83) settitle:@"登录" Font:13 cornerRadius:5 masksToBounds:YES superView:self section:@selector(back) Target:self settitlecolor:[UIColor whiteColor]];
         [_loginbnt mas_makeConstraints:^(MASConstraintMaker *make) {
             make.top.equalTo(self.passimageview.bottom).offset(8);
             make.left.equalTo(30);
@@ -60,5 +47,11 @@
    }
     return self;
 }
-
+-(void)back
+{
+    if (_block)
+    {
+        _block(self);
+    }
+}
 @end
--- 党建e家/党建e家/Tools/Category/Category.h
+++ 党建e家/党建e家/Tools/Category/Category.h
@@ -12,6 +12,7 @@
 #pragma mark - 分类头文件
 #import "UIViewController+UIViewController_Category.h"
 #import "UIButton+UIButton_Category.h"
+#import "UIButton+UIButton_HuangQinCategory.h"
 #import "UIImageView+UIImageView_Category.h"
 #import "UILabel+UILabel_Category.h"
 #endif /* Category_h */
